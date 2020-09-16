d2c59d4ab974ed59d1695fa5da41ead662175f1c
joker <uanhermanus@gmail.com>
2020-09-16 02:57:45 +0200

    update code

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -6,7 +7,7 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: Center(child: Text('Dicee')),
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
@@ -15,9 +16,48 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDieNumber = 1;
+  int rightDieNumber = 1;
+  final Random myRandom = Random();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('images/dice$leftDieNumber.png'),
+              onPressed: () {
+                updateDiceState();
+              },
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('images/dice$rightDieNumber.png'),
+              onPressed: () {
+                updateDiceState();
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void updateDiceState() {
+    setState(() {
+      this.leftDieNumber = (myRandom.nextDouble() * 6 + 1).toInt();
+      this.rightDieNumber = (myRandom.nextDouble() * 6 + 1).toInt();
+    });
+    // print('left:' + (this.leftDieNumber).toString());
+    // print('Right:' + (this.rightDieNumber).toString());
   }
 }
